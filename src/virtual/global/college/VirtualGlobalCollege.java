c707e38fa6d650c4aa12531ecdbc6371a0eee9ab
floawino <violetperez59@yahoo.com>
2018-04-29 15:43:14 +0300

    Initial commit

---
+++ src/virtual/global/college/VirtualGlobalCollege.java
@@ -0,0 +1,16 @@
+/*
+ *  Student Number : 17260
+ *  Student Name : Mirza Usman
+ */
+package virtual.global.college;
+
+public class VirtualGlobalCollege {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
