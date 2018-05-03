6827fd81c7c95a511b69878bd1785ff63282bd41
floawino <violetperez59@yahoo.com>
2018-05-03 23:15:18 +0300

    faculty can filter list their students by selecting course code on combo box

--- src/mizarusman/userinterface/StudentContacts.java
+++ src/mizarusman/userinterface/StudentContacts.java
@@ -8,8 +8,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.text.Document;
 import utilities.DbConnection;
 import utilities.Utils;
 
@@ -28,6 +30,21 @@ public class StudentContacts extends javax.swing.JFrame {
     public StudentContacts(String userID) {
         initComponents();
         this.userId = userID;
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT `course1`, `course2`, `course3` FROM `faculty` WHERE `userID` = ?");
+            preparedStatement.setString(1, this.userId);
+            ResultSet rs = preparedStatement.executeQuery();
+            Vector<String> courses = new Vector<String>();
+            if (rs.next()) {
+                courses.add(rs.getString("course1"));
+                courses.add(rs.getString("course2"));
+                courses.add(rs.getString("course3"));
+            }
+            txtCourseCode.setModel(new javax.swing.DefaultComboBoxModel<>(courses));
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentContacts.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
     }
 
     /**
@@ -41,11 +58,11 @@ public class StudentContacts extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        txtCourseCode = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblContacts = new javax.swing.JTable();
         lblMessage = new javax.swing.JLabel();
         btnview = new javax.swing.JButton();
+        txtCourseCode = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -73,22 +90,20 @@ public class StudentContacts extends javax.swing.JFrame {
             }
         });
 
+        txtCourseCode.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(77, 77, 77))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(27, 27, 27)
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(txtCourseCode, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtCourseCode, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(87, 87, 87)
                         .addComponent(btnview, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
@@ -96,18 +111,22 @@ public class StudentContacts extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(47, 47, 47)
                         .addComponent(lblMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(15, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(59, 59, 59))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
+                .addGap(17, 17, 17)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtCourseCode)
-                    .addComponent(btnview))
+                    .addComponent(btnview)
+                    .addComponent(txtCourseCode, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -119,27 +138,24 @@ public class StudentContacts extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnviewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnviewActionPerformed
-        String courseCode = txtCourseCode.getText().trim();
-        PreparedStatement preparedStatement;
+        String courseCode = txtCourseCode.getSelectedItem().toString();
+        System.out.print(courseCode);
         try {
-            if (!courseCode.isEmpty()) {
-                preparedStatement = conn.prepareStatement("SELECT `course1`, `course2`, `course3` FROM `faculty` WHERE `userID` = ?");
-                preparedStatement.setString(1, this.userId);
-                ResultSet rs = preparedStatement.executeQuery();
-                rs.next();
-                if (courseCode.equals(rs.getString("course1")) || courseCode.equals(rs.getString("course2")) || courseCode.equals(rs.getString("course3"))) {
-                    preparedStatement = conn.prepareStatement("SELECT `userID`, `fname`, `lname`, `email` FROM `students`");
-                    rs = preparedStatement.executeQuery();
-                    // creates and displays the table
-                    tblContacts.setModel(Utils.buildTableModel(rs));
-                } else {
-                    lblMessage.setText("You can only view your students");
-                }
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT `course1`, `course2`, `course3` FROM `faculty` WHERE `userID` = ?");
+            preparedStatement.setString(1, this.userId);
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            if (courseCode.equals(rs.getString("course1")) || courseCode.equals(rs.getString("course2")) || courseCode.equals(rs.getString("course3"))) {
+                preparedStatement = conn.prepareStatement("SELECT `userID`, `fname`, `lname`, `email` FROM `students` INNER JOIN student_courses ON students.userID = student_courses.studentID  WHERE student_courses.courseCode = ?");
+                preparedStatement.setString(1, courseCode);
+                rs = preparedStatement.executeQuery();
+                // creates and displays the table
+                tblContacts.setModel(Utils.buildTableModel(rs));
             } else {
-                lblMessage.setText("Enter a course code");
+                lblMessage.setText("You can only view your students");
             }
         } catch (SQLException ex) {
-            Logger.getLogger(FeesManagement.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(StudentContacts.class.getName()).log(Level.SEVERE, null, ex);
 
         }
     }//GEN-LAST:event_btnviewActionPerformed
@@ -155,7 +171,7 @@ public class StudentContacts extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -186,6 +202,6 @@ public class StudentContacts extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblMessage;
     private javax.swing.JTable tblContacts;
-    private javax.swing.JTextField txtCourseCode;
+    private javax.swing.JComboBox<String> txtCourseCode;
     // End of variables declaration//GEN-END:variables
 }
